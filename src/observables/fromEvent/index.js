c5e56c55d1b9b1ae75e9e34a062b46712d086267
glenn vincent hinks <ghinks@yahoo.com>
2018-07-01 07:44:47 -0400

    talk testing

--- src/observables/fromEvent/index.js
+++ src/observables/fromEvent/index.js
@@ -1,9 +1,15 @@
+const { fromEvent, interval } = rxjs
+const { throttle } = rxjs.operators
 let tracker = document.getElementById('tracker');
-let trackerSource = Rx.Observable.fromEvent(document.getElementById('col1'), 'mousemove')
-let trackerSub = trackerSource.subscribe(e => tracker.innerHTML = e.clientX );
+let tracker2 = document.getElementById('tracker2');
+
+let trackerSource = fromEvent(document.getElementById('col1'), 'mousemove')
+let trackerSub = trackerSource.subscribe(e => tracker.innerHTML = 'X co-ord on move ' + e.clientX )
 
 
 
 
-
+const clicks = fromEvent(document, 'click');
+const result = clicks.pipe(throttle(ev => interval(3000)));
+result.subscribe(e => tracker2.innerHTML = 'Y co-ord throttled on click ' + e.clientY);
 
---
+++ src/observables/fromEvent/readme.md
@@ -0,0 +1,4 @@
+# full disclosure
+
+most of the ideas for this page are from copied from codepen.io
+
